aa51856f8e8cf5e5386d29826ac43a52802a2977
theory friction practice <jackson.brebnor@gmail.com>
2015-07-08 00:27:05 -0400

    alters Player::addComponent to account for component uniqueness

--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -1,7 +1,6 @@
 describe('Player', function() {
     var myPlayer, myBoard, t1, t2, t3, n1, n2, n3;
     beforeEach(function() {
-
         myBoard = new Board(7, 6);
         myPlayer = new Player("John");
         t1 = new Token(myPlayer, "#ff00ff");
@@ -13,15 +12,10 @@ describe('Player', function() {
         n1 = myBoard.columns[2].nodes[3];
         n2 = myBoard.columns[3].nodes[2];
         n3 = myBoard.columns[4].nodes[1];
-
         n1.setNeighbor("rt", n2);
         n2.setNeighbor("lb", n1);
         n1.placeToken(t1);
         n2.placeToken(t2);
-
-
-
-
     });
     describe('init', function() {
         it('initializes with a name', function() {
@@ -34,7 +28,6 @@ describe('Player', function() {
             expect(myPlayer.components).toEqual([]);
         });
     });
-
     describe('#hasComponents', function() {
         it('determines if the player has any components', function() {
             expect(myPlayer.hasComponents()).toBeFalse();
@@ -46,7 +39,6 @@ describe('Player', function() {
             expect(myBoard.columns[3].freeIndex).toBe(4);
         });
     });
-
     // describe('#hasDirectedComponents', function() {
     //     it('determines if the player has components matching the direction of arg component', function() {
     //         var t1 = new Token(myPlayer, "#ff00ff");
@@ -60,26 +52,74 @@ describe('Player', function() {
     //         n3.placeToken(t3);
     //         var myComponent = new Component(n1, n2, "rt");
     //         var my2Component = new Component(n2, n3, "rt")
-
-
     //     });
     // });
-
-    describe('#getComponent', function() {
-        it('returns a new component based on the given nodes and neighborRelation', function() {
-
-
-            // n1.placeToken(t1);
-            // n2.placeToken(t2);
-            // n3.placeToken(t3);
-            console.log(n1);
-            console.log(n2);
-            var myComponent = new Component(n1, n2, "rt");
+    describe('component comparisons', function() {
+        var myComponent, n2n3Component;
+        beforeEach(function() {
+            myComponent = new Component(n1, n2, "rt");
             console.log(myComponent);
-            var resultComponent = myPlayer.getComponent(n1, n2, "rt");
-            console.log(resultComponent);
-            expect(resultComponent).toEqual(myComponent);
+            n2n3Component = new Component(n2, n3, "rt");
+            myPlayer.addComponent(myComponent);
 
         });
+        describe('#getComponent', function() {
+            it('returns a new component based on the given nodes and neighborRelation', function() {
+
+                var resultComponent = myPlayer.getComponent(n1, n2, "rt");
+                expect(resultComponent).toEqual(myComponent);
+            });
+        });
+        describe('#hasDirectedComponents', function() {
+            it('determines if the player currently has any components matching the argument', function() {
+                expect(myPlayer.hasDirectedComponents(n2n3Component)).toBeTrue();
+            });
+        });
+        describe('#addComponent', function() {
+            it('appends a new component to the players component array', function() {
+                expect(myPlayer.components).toContain(myComponent);
+            });
+            it('unionizes new component with intersecting components', function() {
+                myPlayer.addComponent(n2n3Component);
+                var origComp = myPlayer.getOriginalArrayComponent(myComponent);
+                expect(origComp.arity).toBe(3);
+            });
+        });
+        describe('#getDirectedComponents', function() {
+            it('returns an array of components whose direction match that of the argument component', function() {
+                expect(myPlayer.getDirectedComponents(n2n3Component)).toContain(myComponent);
+            });
+        });
+        describe('#hasIntersectingComponents', function() {
+            it('determines if any of the matching directed components intersect with the argument component', function() {
+               expect(myPlayer.hasIntersectingComponents(n2n3Component)).toBeTrue(); 
+            });
+        });
+        describe('#getIntersectingComponents', function() {
+            it('returns an array of intersecting components', function() {
+                expect(myPlayer.getIntersectingComponents(n2n3Component)).toContain(myComponent);
+            });
+        });
+
+        describe('#getOriginalArrayComponent', function() {
+            it('takes a component from a modified array and returns the original component', function() {
+                expect(myPlayer.getOriginalArrayComponent(myComponent)).toBe(myComponent);
+            });
+        });
+
+        describe('unionizeComponents', function() {
+            it('adds nodes from an intersecting component to an already existing component', function() {
+                myPlayer.unionizeComponents(myComponent, n2n3Component);
+                expect(myComponent.arity).toBe(3);
+            });
+       
+        });
+
+        describe('evaluateUniqueness', function() {
+            it('determines if a component warrants a unique entry in the components array ', function() {
+                expect(myPlayer.evaluateUniqueness(n2n3Component)).toBeFalse();
+            });
+        });
+
     });
 });
--- src/Player.js
+++ src/Player.js
@@ -10,16 +10,106 @@ Player.prototype.hasComponents = function() {
 Player.prototype.getComponent = function(n1, n2, relCode) {
     console.log(n1);
     console.log(n2);
-    var tmpComp =  new Component(n1, n2, relCode);
-        
+    var tmpComp = new Component(n1, n2, relCode);
+
     return tmpComp;
 };
 Player.prototype.placeToken = function(board, token, colIndex) {
     try {
-       
-            board.placeToken(token, colIndex);
-        
+
+        board.placeToken(token, colIndex);
+
     } catch (err) {
         alert
     }
+};
+Player.prototype.hasDirectedComponents = function(newComp) {
+    var result;
+    console.log(newComp.direction);
+    if (this.hasComponents() == false) {
+        result = false
+    } else {
+        result = this.components.some(function(dComp, id, arr) {
+            console.log(dComp.direction);
+            return dComp.direction == newComp.direction;
+        }, this);
+    };
+
+    return result;
+};
+
+Player.prototype.addComponent = function(newComp) {
+    if (this.evaluateUniqueness(newComp) == true) {
+        this.components.push(newComp);
+    } else if (this.evaluateUniqueness(newComp) == false) {
+        var iComps = this.getIntersectingComponents(newComp);
+        iComps.forEach(function(iComp, id, arr) {
+            var oIComp = this.getOriginalArrayComponent(iComp);
+            this.unionizeComponents(oIComp, newComp);
+        }, this);
+
+    };
+
+};
+
+Player.prototype.getDirectedComponents = function(newComp) {
+    if (this.hasDirectedComponents(newComp) == true) {
+        return this.components.filter(function(dComp) {
+            return dComp.direction == newComp.direction;
+        });
+    };
+};
+
+Player.prototype.hasIntersectingComponents = function(newComp) {
+    var result;
+    if (this.hasDirectedComponents(newComp) == false) {
+        result = false;
+    } else if (this.hasDirectedComponents(newComp) == true) {
+        var matchingComponents = this.getDirectedComponents(newComp);
+        result = matchingComponents.some(function(dComp, id, arr) {
+            return dComp.intersects(newComp) == true;
+
+        }, this);
+    };
+
+    return result;
+};
+
+Player.prototype.getIntersectingComponents = function(newComp) {
+    var result;
+    if (this.hasDirectedComponents(newComp) == true) {
+        var matchingComponents = this.getDirectedComponents(newComp);
+        var result = matchingComponents.filter(function(dComp, id, arr) {
+            return dComp.intersects(newComp);
+        }, this);
+    } else {
+        result = [];
+    };
+    return result;
+
+};
+
+Player.prototype.getOriginalArrayComponent = function(newComp) {
+    var oIndex = this.components.indexOf(newComp);
+    if (oIndex > -1) {
+        return this.components[oIndex];
+    };
+};
+
+Player.prototype.unionizeComponents = function(oComp, newComp) {
+    oComp.unionize(newComp);
+};
+
+Player.prototype.evaluateUniqueness = function(newComp) {
+    var result;
+    if (this.hasComponents() == false) {
+        result = true;
+    } else if (this.hasDirectedComponents(newComp) == false) {
+        result = true;
+    } else if (this.hasIntersectingComponents(newComp) == false) {
+        result = true;
+    } else if (this.hasIntersectingComponents(newComp) == true) {
+        result = false;
+    };
+    return result;
 };
