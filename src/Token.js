f1decafc84e26d37e9784286c982b612e9b90042
theory friction practice <jackson.brebnor@gmail.com>
2016-06-01 21:45:08 -0400

    jsDoc for C4.Token

--- src/Token.js
+++ src/Token.js
@@ -1,3 +1,9 @@
+/**
+ * a connect four token
+ * @constructor C4.Token
+ * @param {C4.Player} player the player owning the token
+ * @param {Color} color
+ */
 C4.Token = function(player, color) {
     this.player = player;
     this.color = color;
